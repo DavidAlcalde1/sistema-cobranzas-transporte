7371e92ac771b366548be62815757803e3969649
José David Alcalde Cabrera <david20egt@gmail.com>
2026-04-23 16:53:34 -0500

    Hardcodear URL del API para producción

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const API_URL = 'https://site--api-cobranzas--b8r4vf6vdbjv.code.run/api';
 
 const api = axios.create({
   baseURL: API_URL,
